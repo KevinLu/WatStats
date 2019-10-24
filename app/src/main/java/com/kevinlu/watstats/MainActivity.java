c44ad29114577424d61f4fbb50ce76bf51c3aa6a
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2019-10-23 21:06:56 -0400

    Add WebView code to login to WatCard account

--- app/src/main/java/com/kevinlu/watstats/MainActivity.java
+++ app/src/main/java/com/kevinlu/watstats/MainActivity.java
@@ -3,12 +3,36 @@ package com.kevinlu.watstats;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webDriver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webDriver = findViewById(R.id.webview);
+        webDriver.loadUrl("https://watcard.uwaterloo.ca/OneWeb/Account/LogOn");
+        webDriver.getSettings().setJavaScriptEnabled(true);
+        webDriver.setWebViewClient(new WebViewClient() {
+            public void onPageFinished(WebView view, String url) {
+                String user = "watcard number";
+                String pwd = "1234";
+                view.loadUrl("javascript:document.getElementById('Account').value = '" + user + "';document.getElementById('Password').value='" + pwd + "';"+"(function(){ l=document.getElementsByClassName('btn ow-btn-primary btn-block-xs pull-right')[0]; l.click();})()");
+            }
+        });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
