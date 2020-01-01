b2542a0328da0a68a6ed84e8799aed42098274f6
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2019-12-31 21:27:56 -0500

    Add monthly spending statistics to home page

--- app/src/main/java/com/kevinlu/watstats/MainActivity.java
+++ app/src/main/java/com/kevinlu/watstats/MainActivity.java
@@ -1,19 +1,29 @@
 package com.kevinlu.watstats;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import com.francochen.watcard.WatCardClient;
 import com.francochen.watcard.model.balance.BalanceType;
 import com.francochen.watcard.model.balance.Balances;
+import com.francochen.watcard.model.transaction.Transaction;
+import com.francochen.watcard.model.transaction.TransactionRequest;
+import com.francochen.watcard.model.transaction.Transactions;
 import com.kevinlu.watstats.data.AccountBalance;
 import com.kevinlu.watstats.data.MonthlySpending;
 
@@ -27,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager accountBalances;
     private ViewPager monthlySpendings;
     private AccountBalanceAdapter accountBalanceAdapter;
+    MonthlySpendAdapter monthlySpendAdapter;
     private List<AccountBalance> accountBalanceList;
     private List<MonthlySpending> monthlySpendingList;
     private WatCardClient.Builder builder = new WatCardClient.Builder();
@@ -35,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     private String pass;
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
 
         monthlySpendingList = new ArrayList<>();
 
-        MonthlySpendAdapter monthlySpendAdapter = new MonthlySpendAdapter(monthlySpendingList, this);
+        monthlySpendAdapter = new MonthlySpendAdapter(monthlySpendingList, this);
 
         monthlySpendings = findViewById(R.id.monthly_spend_pager);
         monthlySpendings.setAdapter(monthlySpendAdapter);
@@ -91,6 +103,9 @@ public class MainActivity extends AppCompatActivity {
         client = builder.build();
 
         getBalances();
+        // Currently the default is the last 3 months of spending.
+        // In the future, it's possible to add a settings page to change this.
+        getMonthlySpending(3);
     }
 
     public void getBalances() {
@@ -109,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
                         for (BalanceType balanceType : BalanceType.values()) {
                             sum += Float.valueOf(balances.get(balanceType).getBalance().toString());
                         }
-                        String total = "$ " + sum;
+                        String total = "$ " + roundUp(sum, 2);
                         setTotalBalance(total);
                     }
 
@@ -118,6 +133,62 @@ public class MainActivity extends AppCompatActivity {
                         Log.e("Watcard:getBalances()", e.toString());
                     }
                 }));
+    }
+
+    /**
+     * Gets the amount of money spent however many {@code monthsAgo}.
+     *
+     * @param monthsAgo an integer, how many months ago of spending to show.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void getMonthlySpending(int monthsAgo) {
+        LocalDate now = LocalDate.now();
+        for (int i = 0; i < monthsAgo; i++) {
+            YearMonth month = YearMonth.from(now.minusMonths(i));
+            LocalDate from = month.atDay(1);
+            LocalDate to = month.atEndOfMonth();
+            setMonthlySpendingList(from, to);
+        }
+    }
+
+    /**
+     * Populates the monthlySpendingList with the amount of money in the specified time period.
+     * Date period should be from the start of a month to the end of a month.
+     *
+     * @param from a LocalDate that sets the date from query
+     * @param to   a LocalDate that sets the date to query
+     */
+    public void setMonthlySpendingList(LocalDate from, LocalDate to) {
+        compositeDisposable.add(client.getTransactions(new TransactionRequest(from, to))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableSingleObserver<Transactions>() {
+                    @RequiresApi(api = Build.VERSION_CODES.O)
+                    @Override
+                    public void onSuccess(Transactions transactions) {
+                        float monthlySpending = 0;
+                        for (Transaction transaction : transactions.get()) {
+                            monthlySpending += transaction.getAmount().floatValue();
+                        }
+                        String totalMonthlySpending = "$ " + roundUp(monthlySpending, 2);
+                        setMonthlySpending(totalMonthlySpending, from.getMonth().getDisplayName(TextStyle.FULL, Locale.CANADA));
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("Watcard:getMonthlySpend", e.toString());
+                    }
+                }));
+    }
+
+    /**
+     * Adds a MonthlySpending object to the monthlySpendingList.
+     *
+     * @param monthlySpending a String, the amount of money spent in that month.
+     * @param month           a String, the month spending occurred.
+     */
+    public void setMonthlySpending(String monthlySpending, String month) {
+        monthlySpendingList.add(new MonthlySpending(month, monthlySpending));
+        monthlySpendAdapter.notifyDataSetChanged();
     }
 
     public void setTotalBalance(String total) {
@@ -132,6 +203,12 @@ public class MainActivity extends AppCompatActivity {
         accountBalanceList.add(new AccountBalance(R.drawable.ic_transfer, "Transfer Meal Plan",
                 transferAmount, "$ 0"));
         accountBalanceAdapter.notifyDataSetChanged();
+    }
+
+    public float roundUp(float d, int decimalPlace) {
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd.floatValue();
     }
 
     public int dpToPx(int dp) {
